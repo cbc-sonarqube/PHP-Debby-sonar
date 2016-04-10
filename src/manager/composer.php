d6d682946006bfe48a40bb8883963456afa19806
Lode Claassen <lode@alsvanzelf.nl>
2016-04-10 20:24:49 +0200

    skip composer check failure on platform packages

--- src/manager/composer.php
+++ src/manager/composer.php
@@ -84,6 +84,11 @@ public function find_required_packages() {
 		
 		$this->required = [];
 		foreach ($composer_json['require'] as $package_name => $required_version) {
+			// skip platform packages like 'ext-curl'
+			if (strpos($package_name, '/') === false) {
+				continue;
+			}
+			
 			$package = $this->get_package_by_name($package_name);
 			$package->mark_required($required_version);
 			
